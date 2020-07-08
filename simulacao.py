6a287a01927098e232f9d7e06f0b2d35a8526b1c
A. Nelson <65572920+anelsonsl@users.noreply.github.com>
2020-07-08 19:13:28 +0200

    08/07/20

--- simulacao.py
+++ simulacao.py
@@ -8,17 +8,27 @@ def linha():
 agora=datetime.now()
 
 print(f'O vavlor agora : {agora}. do tipo {type(agora)}')
-es=1 #Entrada ao servico 1; Saida servico 2; Entrada Pausa 3. Saida da Pauda 4.
+es=1 #Entrada ao servico 1; Saida servico 2; Entrada Pausa 3. Saida da Pausa 4.
 bemerkung='xxxxxxxxx'
 regkart=[agora,es,bemerkung]
 linha()
 print(regkart)
 linha()
 
-ficheiro=open('simulacao.pkl','wb')
+#Escrever ficheiro com 31 registo
+ficheiro=open('simulacao.pkl','ab')
 for i in range(1,32):
     agora=datetime.now()
-    pickle.dump(regkart)
+    print(agora)
+    pickle.dump(regkart,ficheiro)
+ficheiro.close()
+linha()
+#Ler ficheiro
+regkart=[agora,2,'anular a variavel']
+ficheiro=open('simulacao.pkl','rb')
+teste=pickle.load(ficheiro)
+print(teste)
+ficheiro.close()
 
 
 
